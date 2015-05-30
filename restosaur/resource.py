fee8710d7564578c9ec41c37abfd5d9b6eb53559
Marcin Nowak <marcin.j.nowak@gmail.com>
2015-05-30 21:15:26 +0200

    moved http response factory outside Response class

--- restosaur/resource.py
+++ restosaur/resource.py
@@ -3,6 +3,8 @@ import logging
 import mimeparse
 import responses
 import urltemplate
+from collections import OrderedDict
+
 from .serializers import default_serializers
 from .headers import normalize_header_name
 from .context import Context
@@ -11,11 +13,55 @@ from .context import Context
 log = logging.getLogger(__name__)
 
 
+def http_response(response):
+    """
+    RESTResponse -> HTTPResponse factory
+    """
+
+    from django.http import HttpResponse
+    from .headers import parse_accept_header, build_content_type_header
+
+    context = response.context
+
+    content_type = None
+    serializer = None
+    representation = None
+
+    if 'accept' in context.headers:
+        accepting = parse_accept_header(context.headers['accept'])
+        for content_type, representation, q in accepting:
+            if content_type == '*/*' or content_type == 'application/*':
+                content_type = 'application/json'
+            if context.resource.serializers.contains(content_type)\
+                and (not representation or representation in context.resource.representations):
+                try:
+                    representation = representation or context.resource.representations.keys()[0]
+                except IndexError:
+                    pass
+                serializer = context.resource.serializers[content_type]
+                content_type = build_content_type_header(content_type, representation)
+                break
+    else:
+        content_type = 'application/json'
+
+    if not content_type or not representation or not serializer:
+        return HttpResponse(status=406) # Not Acceptable
+
+    if response.data is not None:
+        content = response.serialize(serializer, response.data, representation)
+    else:
+        content = ''
+
+    httpresp = HttpResponse(content, status=response.status)
+    httpresp['Content-Type'] = content_type
+    return httpresp
+
+
 class Resource(object):
     def __init__(self, path, serializers=None):
         self._path = path
         self._callbacks = {}
-        self._representations = {}
+        self._representations = OrderedDict()
         self._serializers = serializers or default_serializers
         # register aliases for the decorators
         for verb in ('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'):
@@ -49,7 +95,7 @@ class Resource(object):
                 if request.body:
                     ctx.body = self._serializers[mimetype].loads(request.body)
             else:
-                return ctx.NotAcceptable()
+                return http_response(ctx.NotAcceptable())
 
         ctx.content_type = request.META['CONTENT_TYPE']
 
@@ -63,9 +109,9 @@ class Resource(object):
         log.debug('Calling %s, %s, %s' % (method, args, kw))
         if method in self._callbacks:
             resp = self._callbacks[method](ctx, *args, **kw)
-            return resp
+            return http_response(resp)
         else:
-            return ctx.MethodNotAllowed()
+            return http_response(ctx.MethodNotAllowed())
 
     def representation(self, name='default'):
         def wrapped(func):
@@ -80,4 +126,16 @@ class Resource(object):
         uri = context.build_absolute_uri(self._path)
 
         return urltemplate.to_url(uri, params)
+
+    def convert(self, context, obj, representation=None):
+        """
+        Converts model (`obj`) using specified or default `representation`
+        within a `context`
+        """
+
+        if representation is None:
+            convert = self.representations.values()[0]
+        else:
+            convert = self.representations[representation]
+        return convert(obj, context)
 
--- restosaur/responses.py
+++ restosaur/responses.py
@@ -1,48 +1,27 @@
-from django.http import HttpResponse
-from .headers import parse_accept_header, build_content_type_header
 from .utils import model_to_dict
 
 
-class Response(HttpResponse):
-    def __init__(self, context, data=None, *args, **kwargs):
+class Response(object):
+    def __init__(self, context, data=None, status=200, headers=None):
+        self.headers = {}
+        self.headers.update(headers or {})
         self.representation = None
         self.content_type = None
-
-        if 'accept' in context.headers:
-            accepting = parse_accept_header(context.headers['accept'])
-            for content_type, representation, q in accepting:
-                if content_type == '*/*' or content_type == 'application/*':
-                    content_type = 'application/json'
-                if context.resource.serializers.contains(content_type)\
-                    and (not representation or representation in context.resource.representations):
-                    self.representation = representation or 'default'
-                    self.serializer = context.resource.serializers[content_type]
-                    self.content_type = build_content_type_header(content_type, representation)
-                    break
-        else:
-            self.content_type = 'application/json'
-
-
-        if not self.content_type or not self.representation:
-            super(Response, self).__init__(status=406) # Not Acceptable
-            return
-
-        super(Response, self).__init__(*args, **kwargs)
-
-        self.serializer = context.resource.serializers[content_type]
         self.context = context
-        self['Content-Type'] = self.content_type
-
-        if data:
-            self.set_data(data)
-
-    def set_data(self, data):
-        self.content = self.serializer.dumps(data)
+        self.status = status
+        self.data = data
 
     def get_converter(self, representation):
         dummy_converter = lambda x, context: x
         converter = self.context.resource.representations.get(representation)
         return converter or dummy_converter
+
+    def serialize(self, serializer, data, representation):
+        if data is None:
+            return ''
+        else:
+            output = self.context.resource.convert(self.context, data, representation)
+            return serializer.dumps(output)
 
 
 class CreatedResponse(Response):
@@ -77,25 +56,20 @@ class MethodNotAllowedResponse(Response):
 
 class CollectionResponse(Response):
     def __init__(self, context, iterable, totalCount=None, key=None):
-        super(CollectionResponse, self).__init__(context)
-        key = key or 'items'
-        resp = {
-                key: self.get_items(iterable),
-                'totalCount': totalCount if totalCount is not None else len(iterable),
-                }
-        self.set_data(resp)
+        super(CollectionResponse, self).__init__(context, data=iterable)
+        self.key = key or 'items'
+        self.totalCount = totalCount
 
-    def get_items(self, iterable):
-        dummy_converter = lambda x, context: x
-        converter = self.get_converter(self.representation) or dummy_converter
-        return list(map(lambda x: converter(x, self.context), iterable))
+    def serialize(self, serializer, iterable, representation):
+        resp = {
+                self.key: map(lambda x: self.context.resource.convert(self.context, x, representation), iterable),
+                'totalCount': self.totalCount if self.totalCount is not None else len(iterable),
+                }
+        return serializer.dumps(resp)
 
 
 class EntityResponse(Response):
-    def set_data(self, data):
-        convert = self.get_converter(self.representation)
-        content = convert(data, self.context)
-        super(EntityResponse, self).set_data(content)
+    pass
 
 
 class NotAcceptableResponse(Response):
