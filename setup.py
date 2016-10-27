154f7e06693713b145fb6b894c39e60fa4a491ed
Marcin Nowak <marcin.j.nowak@gmail.com>
2016-10-27 12:20:45 +0200

    version bump (beta 1)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='restosaur',
-      version='0.7.0',
+      version='0.7.0b1',
       description='Damn simple RESTful library',
       classifiers=[
         "Development Status :: 3 - Alpha",
