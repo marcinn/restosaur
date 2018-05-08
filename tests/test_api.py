4955dd5264f39d5f7e44323b85aeca19ebc7b4b4
Marcin Nowak <marcin.j.nowak@gmail.com>
2018-05-08 15:04:01 +0200

    (tests) fix tests for django 2.0

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,13 @@ from restosaur.contrib.django.dispatch import resource_dispatcher_factory
 from .utils import response_content_as_text
 
 
+def _urlpattern_regex(urlpattern):
+    try:
+        return urlpattern._regex  # django < 2.0
+    except AttributeError:
+        return urlpattern.pattern._regex  # django 2.0
+
+
 class APITestCase(unittest.TestCase):
     def setUp(self):
         from django.test import RequestFactory
@@ -34,14 +41,14 @@ class APIPathsTestCase(APITestCase):
         api.resource('/')
         urls = api.get_urls()
         root_url = urls[0].url_patterns[0]
-        self.assertFalse('/' in root_url._regex)
+        self.assertFalse('/' in _urlpattern_regex(root_url))
 
     def test_that_typical_url_pattern_does_not_contain_prepending_slash(self):
         api = DjangoAPI('foo')
         api.resource('bar/')
         urls = api.get_urls()
         bar_url = urls[0].url_patterns[0]
-        self.assertEqual(bar_url._regex, '^bar/$')
+        self.assertEqual(_urlpattern_regex(bar_url), '^bar/$')
 
     def test_status_200OK_of_nonprefixed_api_path(self):
         api = API()
